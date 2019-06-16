fd31114d6a3116540de0687c929154d61d32aa18
Srinivas Narayana <ngsrinivas@gmail.com>
2019-06-16 10:55:39 +0530

    Initial commit with example usage and data files

---
+++ pubplot-example.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+plotscript="pubplot.py"
+plotpath="."
+outfile="elapsed-vs-load.eps"
+datafile1="data/loadsxtimeatpriority19.csv"
+datafile2="data/loadsxtimeatpriority18.csv"
+python $plotscript -g -a "font \"Helvetica,24\"" \
+    -p 2 -f "postscript enhanced color" \
+    -o $outfile \
+    -x "background load" -y "Elapsed ns" \
+    --bmargin 5.2 --rmargin 4.3 \
+    --csv \
+    $datafile1 "prio 19" 1:2 \
+    $datafile2 "prio 18" 1:2 \
+    | gnuplot ; epstopdf $plotpath/$outfile --autorotate=All
